c8f67269779b62e3d85c7ff27422eb8268a26685
Eric <erixilva42@gmail.com>
2022-09-16 20:10:27 -0300

    BaSP-W05-2022 js files and excercise 1

---
+++ week 05/excercises/01_variables_and_operators.js
@@ -0,0 +1,24 @@
+console.log('Excercise 01: variables and operators')
+
+// Crear dos variables numéricas y utilizar el operador suma
+// para guardar el valor de la suma de ambos números en una 3er variable.
+console.log('Excercise 1a')
+var num1 = 42000;
+var num2 = 6900;
+var sum = num1 + num2;
+console.log('Sum of 42000 and 6900 is: ' + sum);
+
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+console.log('Excercise 1b');
+var weather1 = 'cloudy ';
+var weather2 = 'with a chance of meatballs';
+var forecast = weather1 + weather2;
+console.log('The weather today is: ' + forecast);
+
+// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+// guardando el resultado de la suma en una 3er variable (utilizar length).
+console.log('Excercise 1c');
+var text1 = 'blablabla';
+var text2 = 'blebleble';
+var textsLenght = text1.length + text2.length;
+console.log('The total amount of characters is: ' + textsLenght);
